45504c50e2377ab7b968b523820986d3192a442b
jorabek27 <rashidovjoabek@gmail.com>
2025-07-08 21:44:23 +0500

    uzum 8 july

--- src/Components/Uzum/Topshirishpunktiochish/Openpunkt.jsx
+++ src/Components/Uzum/Topshirishpunktiochish/Openpunkt.jsx
@@ -50,7 +50,9 @@ function Openpunkt() {
             >
               Uzum Market topshirish punktini oching
             </p>
-            <p style={{ fontSize: "20px", color: "#fff" , paddingBottom:"40px"}}>
+            <p
+              style={{ fontSize: "20px", color: "#fff", paddingBottom: "40px" }}
+            >
               Ishga tushirishga yordam beramiz va xodimlarni o‘qitamiz
             </p>
             <p
@@ -63,47 +65,251 @@ function Openpunkt() {
               }}
             >
               <img
-                style={{ width: "44px", height: "44px" ,marginBottom:"10px"}}
+                style={{ width: "44px", height: "44px", marginBottom: "10px" }}
                 src="https://uzum.uz/landings/_ipx/s_88x88/landings/img/pvz/icons/money.png"
                 alt=""
               />
               Oyiga 15 mln so‘mdan boshlab mukofotlash
             </p>
-            <p style={{
+            <p
+              style={{
                 display: "flex",
                 gap: "10px",
                 alignItems: "center",
                 color: "#Fff",
                 fontSize: "18px",
-              }}>
-              <img style={{ width: "44px", height: "44px" ,marginBottom:"10px"}}
+              }}
+            >
+              <img
+                style={{ width: "44px", height: "44px", marginBottom: "10px" }}
                 src="	https://uzum.uz/landings/_ipx/s_88x88/landings/img/pvz/icons/hand-money.png"
                 alt=""
               />
               Sarmoya 40 mln so‘mdan boshlab
             </p>
-            <p style={{
+            <p
+              style={{
                 display: "flex",
                 gap: "10px",
                 alignItems: "center",
                 color: "#Fff",
                 fontSize: "18px",
-              }}>
-              <img style={{ width: "44px", height: "44px" ,marginBottom:"10px"}}
+              }}
+            >
+              <img
+                style={{ width: "44px", height: "44px", marginBottom: "10px" }}
                 src="	https://uzum.uz/landings/_ipx/s_88x88/landings/img/pvz/icons/up-arrow.png"
                 alt=""
               />
               O‘zini qoplash 12 oydan boshlab{" "}
             </p>
-            <button className="aloqa">
+            <button
+              style={{ color: "rgba(0, 0, 0, 0.819)", background: "#fff" }}
+              className="aloqa"
+            >
               Aloqa uchun ma`lumot qoldirish
             </button>
           </div>
           <div className="imge">
-            <img 
+            <img
               src="https://uzum.uz/landings/_ipx/_/landings/img/pvz/main-image.png"
               alt=""
             />
+          </div>
+        </div>
+      </div>
+      <div className="useful">
+        <h1 style={{ fontSize: "36px" }}>Bu nima uchun foydali</h1>
+        <div
+          style={{ display: "flex", gap: "10px", marginTop: "35px" }}
+          className="tree-boxes"
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "250px",
+              background: "#f0f2f5",
+              borderRadius: "32px",
+              padding: "20px 24px",
+            }}
+            className="box"
+          >
+            <img
+              style={{ width: "74px", height: "74px", marginBottom: "10px" }}
+              src="	https://uzum.uz/landings/_ipx/s_148x148/landings/img/pvz/icons/money-lg.png"
+              alt=""
+            />
+            <h3
+              style={{
+                fontSize: "18px",
+                color: "#1f2026",
+                marginBottom: "6px",
+              }}
+            >
+              Birinchi ish oyining o‘zidan daromad
+            </h3>
+            <p style={{ fontSize: "14px", color: "#4d4f59" }}>
+              Barqaror qo‘shimcha daromadga ega bo‘ling
+            </p>
+          </div>
+          <div
+            style={{
+              width: "100%",
+              height: "250px",
+              background: "#f0f2f5",
+              borderRadius: "32px",
+              padding: "20px 24px",
+            }}
+            className="box"
+          >
+            <img
+              style={{ width: "74px", height: "74px", marginBottom: "10px" }}
+              src="	https://uzum.uz/landings/_ipx/s_148x148/landings/img/pvz/icons/money-lg.png"
+              alt=""
+            />
+            <h3
+              style={{
+                fontSize: "18px",
+                color: "#1f2026",
+                marginBottom: "6px",
+              }}
+            >
+              Birinchi ish oyining o‘zidan daromad
+            </h3>
+            <p style={{ fontSize: "14px", color: "#4d4f59" }}>
+              Barqaror qo‘shimcha daromadga ega bo‘ling
+            </p>
+          </div>
+          <div
+            style={{
+              width: "100%",
+              height: "250px",
+              background: "#f0f2f5",
+              borderRadius: "32px",
+              padding: "20px 24px",
+            }}
+            className="box"
+          >
+            <img
+              style={{ width: "74px", height: "74px", marginBottom: "10px" }}
+              src="	https://uzum.uz/landings/_ipx/s_148x148/landings/img/pvz/icons/money-lg.png"
+              alt=""
+            />
+            <h3
+              style={{
+                fontSize: "18px",
+                color: "#1f2026",
+                marginBottom: "6px",
+              }}
+            >
+              Birinchi ish oyining o‘zidan daromad
+            </h3>
+            <p style={{ fontSize: "14px", color: "#4d4f59" }}>
+              Barqaror qo‘shimcha daromadga ega bo‘ling
+            </p>
+          </div>
+        </div>
+        <button
+          style={{ color: "#fff", background: "#7000ff" }}
+          className="aloqa"
+        >
+          Aloqa uchun ma`lumot qoldirish
+        </button>
+      </div>
+      <div className="easy-to-star">
+        <h2 style={{ fontSize: "36px", marginBottom: "40px" }}>
+          Oson boshlash
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="nuber">1</div>
+            <h2>Joyni toping </h2>
+            <p>
+              Biznesingiz hududidan foydalanishingiz yoki yangi joy ijaraga
+              olishingiz mumkin
+            </p>
+          </div>
+          <div className="card">
+            <div className="nuber">1</div>
+            <h2>Joyni toping </h2>
+            <p>
+              Biznesingiz hududidan foydalanishingiz yoki yangi joy ijaraga
+              olishingiz mumkin
+            </p>
+          </div>
+          <div className="card">
+            <div className="nuber">1</div>
+            <h2>Joyni toping </h2>
+            <p>
+              Biznesingiz hududidan foydalanishingiz yoki yangi joy ijaraga
+              olishingiz mumkin
+            </p>
+          </div>
+          <div className="card">
+            <div className="nuber">1</div>
+            <h2>Joyni toping </h2>
+            <p>
+              Biznesingiz hududidan foydalanishingiz yoki yangi joy ijaraga
+              olishingiz mumkin
+            </p>
+          </div>
+          <button
+            style={{ color: "#fff", background: "#7000ff" }}
+            className="aloqa"
+          >
+            Aloqa uchun ma`lumot qoldirish
+          </button>
+        </div>
+        <h2
+          style={{ fontSize: "36px", marginBottom: "40px", marginTop: "60px" }}
+        >
+          Nimalarni tayyyorlash kerak
+        </h2>
+        <div className="cards-three">
+          <div className="card">
+            <h2>Joy </h2>
+            <p>
+              Buyurtmalar topshirish zonasi sig‘ishiga 20-50 m² joy mos keladi
+            </p>
+          </div>
+          <div className="card">
+            <h2>Joy</h2>
+            <p>
+              Buyurtmalar topshirish zonasi sig‘ishiga 20-50 m² joy mos keladi
+            </p>
+          </div>{" "}
+          <div className="card">
+            <h2>Joy </h2>
+            <p>
+              Buyurtmalar topshirish zonasi sig‘ishiga 20-50 m² joy mos keladi
+            </p>
+          </div>
+        </div>
+        <div className="why-with-we">
+          <h2
+            style={{
+              fontSize: "36px",
+              marginBottom: "40px",
+              marginTop: "40px",
+            }}
+          >
+            Nega aynan bizni tanlash kerak
+          </h2>
+          <div className="cards">
+            <div className="card">
+              <h3 style={{fontSize:"38px",color:"#7f4dff",}}>1.2 mln ta</h3>
+              <p style={{fontSize:'18px',color:'black',marginTop:'25px'}}>mijozlar</p>
+            </div>  <div className="card">
+              <h3 style={{fontSize:"38px",color:"#7f4dff",}}>1.2 mln ta</h3>
+              <p style={{fontSize:'18px',color:'black',marginTop:'25px'}}>mijozlar</p>
+            </div>  <div className="card">
+              <h3 style={{fontSize:"38px",color:"#7f4dff",}}>1.2 mln ta</h3>
+              <p style={{fontSize:'18px',color:'black',marginTop:'25px'}}>mijozlar</p>
+            </div>  <div className="card">
+              <h3 style={{fontSize:"38px",color:"#7f4dff",}}>1.2 mln ta</h3>
+              <p style={{fontSize:'18px',color:'black',marginTop:'25px'}}>mijozlar</p>
+            </div>
+          
           </div>
         </div>
       </div>
